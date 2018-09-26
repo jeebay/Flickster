2ff59a0aaca69a7b379a026b71cdfa8b8ca7b2ab
Brian Johnson <bjohnson@hearsaycorp.com>
2018-09-25 17:23:32 -0700

    Add view holder pattern to array adapter

--- app/src/main/java/com/bjohnson/flickster/Adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/bjohnson/flickster/Adapters/MovieArrayAdapter.java
@@ -22,27 +22,39 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         super(context, android.R.layout.simple_list_item_1, movies);
     }
 
+    private static class ViewHolder {
+        TextView title;
+        TextView description;
+        ImageView poster;
+    }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Movie movie = getItem(position);
 
+        ViewHolder viewHolder;
         if(convertView == null) {
+            viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_movie, parent, false);
+
+            viewHolder.poster = convertView.findViewById(R.id.ivMovieImage);
+            viewHolder.title = convertView.findViewById(R.id.tvTitle);
+            viewHolder.description = convertView.findViewById(R.id.tvOverview);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
 
-        ImageView ivImage = convertView.findViewById(R.id.ivMovieImage);
-        TextView tvTitle = convertView.findViewById(R.id.tvTitle);
-        TextView tvOverview = convertView.findViewById(R.id.tvOverview);
-
-        ivImage.setImageResource(0);
-        tvTitle.setText(movie.getOrigialTitle());
-        tvOverview.setText(movie.getOverview());
+        viewHolder.poster.setImageResource(0);
+        viewHolder.title.setText(movie.getOrigialTitle());
+        viewHolder.description.setText(movie.getOverview());
 
         Picasso.with(getContext()).load(movie.getPosterPath()).
-                fit().centerCrop().placeholder(R.drawable.placeholder).into(ivImage);
+                fit().centerCrop().placeholder(R.drawable.placeholder).into(viewHolder.poster);
         return convertView;
     }
 }
